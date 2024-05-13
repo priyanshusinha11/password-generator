f6adc9112bafac8f589cfef35766076b7228b3e4
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-05-13 23:44:44 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-900 text-orange-500">
-        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <h1 className='text-white text-center my-3 text-2xl font-bold'>Password generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
